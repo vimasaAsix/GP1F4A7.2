8f15c39470ab3c6d0dfe4f45db5d4c6a4a2a1c27
vimasaAsix <vsaulm15@gmail.com>
2023-10-31 18:27:16 +0100

    Vagrant GP1F4A7

---
+++ Codi/CostFabLlauBeg.php
@@ -0,0 +1,45 @@
+<?php
+	
+	class llauna {
+		
+		private $radi;
+		private $logintud;
+		private $gruix;
+		private $densitat;
+		private $costpergram;
+		
+		public function __construct($r,$l,$g,$d,$cpg){
+			$this->radi = $r;
+			$this->longitud = $l;
+			$this->gruix = $g;
+			$this->densitat = $d;
+			$this->costpergram = $cpg;
+		}
+		
+		private function volum_material_llauna_cm3(){
+			$ab = M_PI * $this->radi**2;
+			$al = 2 * M_PI * $this->radi * $this->longitud;
+			$volum = (2*$ab + $al) * $this->gruix; 
+			return $volum;		
+ 		}
+ 		
+ 		private function massa_material_llauna_grams(){
+			$massa = $this->volum_material_llauna_cm3() * $this->densitat;
+			return $massa;		
+ 		}
+ 		
+ 		private function cost_fabricacio_llauna_en_dolars(){
+			$cost = $this->massa_material_llauna_grams() * $this->costpergram; 
+			return $cost;			
+ 		}
+ 		
+ 		public function mostra_cost_fabricacio_llauna_en_dolars(){
+			$mostra_cost = "El cost de fabricació de la llauna de beguda en $ és: ".$this -> cost_fabricacio_llauna_en_dolars()."$<br>"; 
+			return $mostra_cost;			
+ 		}	
+	}
+	$llauna_beguda = new llauna($_GET['r'],$_GET['l'],$_GET['g'],$_GET['d'],$_GET['cpg']);
+	echo $llauna_beguda->mostra_cost_fabricacio_llauna_en_dolars();
+?>
+	
+	
